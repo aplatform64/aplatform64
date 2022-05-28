9376774c8c7dbacc746b89d9e783694d937e35c2
serdigital64 <serdigital64@gmail.com>
2022-05-28 18:07:11 -0300

    feat: add new CLI

---
+++ src/cli/ap64-env.bash
@@ -0,0 +1,14 @@
+#
+###[ embedded-bashlib64-end ]#####################
+#
+
+# Shell script name
+readonly AP64_CLI='ap64'
+
+# Install mark. Mapped to: auto_ansible_control_base['files']['cli_root']
+readonly AP64_CLI_ROOT='.ap64-root'
+
+# A:Platform64 paths
+declare AP64_ROOT=''
+declare AP64_VAR=''
+declare AP64_INVENTORY=''
