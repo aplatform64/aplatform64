06869fc0ae1e222d2a2d59b17c4eca3b1e204adf
SerDigital64 (Max) <serdigital64@gmail.com>
2024-02-05 18:33:12 +0100

    dev: update cicd (#12)

--- src/cli/env.bash
+++ src/cli/env.bash
@@ -19,7 +19,28 @@ readonly AP64_VENV='venv'
 # A:Platform64
 #
 
-# paths
+# Collections
+declare AP64_NAMESPACE='serdigital64'
+declare AP64_COLLECTIONS=''
+AP64_COLLECTIONS+=" ${AP64_NAMESPACE}.application"
+AP64_COLLECTIONS+=" ${AP64_NAMESPACE}.automation"
+AP64_COLLECTIONS+=" ${AP64_NAMESPACE}.backup"
+AP64_COLLECTIONS+=" ${AP64_NAMESPACE}.cloud"
+AP64_COLLECTIONS+=" ${AP64_NAMESPACE}.container"
+AP64_COLLECTIONS+=" ${AP64_NAMESPACE}.database"
+AP64_COLLECTIONS+=" ${AP64_NAMESPACE}.development"
+AP64_COLLECTIONS+=" ${AP64_NAMESPACE}.devops"
+AP64_COLLECTIONS+=" ${AP64_NAMESPACE}.hardware"
+AP64_COLLECTIONS+=" ${AP64_NAMESPACE}.hypervisor"
+AP64_COLLECTIONS+=" ${AP64_NAMESPACE}.infrastructure"
+AP64_COLLECTIONS+=" ${AP64_NAMESPACE}.monitor"
+AP64_COLLECTIONS+=" ${AP64_NAMESPACE}.network"
+AP64_COLLECTIONS+=" ${AP64_NAMESPACE}.security"
+AP64_COLLECTIONS+=" ${AP64_NAMESPACE}.storage"
+AP64_COLLECTIONS+=" ${AP64_NAMESPACE}.system"
+AP64_COLLECTIONS+=" ${AP64_NAMESPACE}.web"
+
+# Paths
 declare AP64_PATH_ROOT=''
 declare AP64_PATH_VAR=''
 declare AP64_PATH_INVENTORY=''
@@ -27,14 +48,14 @@ declare AP64_PATH_VENV=''
 declare AP64_PATH_VENV_CACHE=''
 declare AP64_PATH_VENV_TMP=''
 
-# file names
+# File names
 declare AP64_FILE_ANSIBLE_CONTROL='ansible_control.env'
 declare AP64_FILE_ANSIBLE_INVENTORY='aplatform64_service.ini'
 declare AP64_FILE_CATALOG='catalog.csv'
 
-# node bootstrap SSH credentials
+# Node bootstrap SSH credentials
 export AP64_NODE_USER
 export AP64_NODE_PASSWORD
 
-# current site
+# Current site
 declare AP64_SITE_CURRENT=''
